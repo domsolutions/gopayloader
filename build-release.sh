7e60e13f377ba12ee9f296771c1bb35ede5f4fc9
dominicriordan <d.riordan@sensysgatso.com>
2023-08-21 17:00:33 +0100

    build output to ./build

--- build-release.sh
+++ build-release.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+cd build
 rm -rf *.tar.gz && rm -rf gopayloader-*
 
-GOOS=windows go build -o gopayloader-windows-amd64.exe ./ && tar -czvf gopayloader-windows-amd64.tar.gz gopayloader-windows-amd64.exe
-GOOS=linux go build -o gopayloader-linux-amd64 ./ && tar -czvf gopayloader-linux-amd64.tar.gz gopayloader-linux-amd64
-GOOS=darwin go build -o gopayloader-darwin-amd64 ./ && tar -czvf gopayloader-darwin-amd64.tar.gz gopayloader-darwin-amd64
+cd ..
+GOOS=windows go build -o ./build/gopayloader-windows-amd64.exe ./ && tar -czvf ./build/gopayloader-windows-amd64.tar.gz ./build/gopayloader-windows-amd64.exe
+GOOS=linux go build -o ./build/gopayloader-linux-amd64 ./ && tar -czvf ./build/gopayloader-linux-amd64.tar.gz ./build/gopayloader-linux-amd64
+GOOS=darwin go build -o ./build/gopayloader-darwin-amd64 ./ && tar -czvf ./build/gopayloader-darwin-amd64.tar.gz ./build/gopayloader-darwin-amd64
+rm -rf ./build/*-amd64 ./build/*-amd64.exe
